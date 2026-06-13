346aae90de008aef3891cb893db4436a6c6795ec
zishanx <zishanalikhan53@gmail.com>
2026-06-13 23:15:54 +0530

    initail Next.js setup with home and about page

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About(){
+    return(
+        <div className="p-10">
+            <h1 className="text-4xl font-bold">About page</h1>
+            <p className="mt-4 text-gray-400">This is the about page.</p>
+        </div>
+    )
+}
