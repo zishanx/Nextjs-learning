89ebd39162d992e4bcca01daf678cfd7c52f7567
zishanx <zishanalikhan53@gmail.com>
2026-06-14 22:53:56 +0530

    Learning dynamic routing

---
+++ app/posts/[id]/page.tsx
@@ -0,0 +1,9 @@
+type PageProps = {
+    params: Promise<{ id: string }>
+}
+
+export default async function PostPage({ params }: PageProps) {
+
+    const {id} = await params;
+    return <h1>Post ID: {id}</h1>
+}
