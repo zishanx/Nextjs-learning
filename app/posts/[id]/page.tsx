467f3a21cb483c42a7514bdde2131fe56dc1bb3f
zishanx <zishanalikhan53@gmail.com>
2026-06-17 20:00:00 +0530

    Your commit message

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -4,6 +4,13 @@ type PageProps = {
 
 export default async function PostPage({ params }: PageProps) {
 
-    const {id} = await params;
-    return <h1>Post ID: {id}</h1>
+    const { id } = await params;
+    return (
+        <>
+            <h1>Post ID: {id}</h1>
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, consequatur aliquam repellendus nihil aut sint odio officia odit cupiditate, consequuntur veritatis amet reiciendis.</h2>
+        </>
+
+    )
+
 }
