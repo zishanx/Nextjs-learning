1aeacd0cc370d4c1e61e542123f3a269341dc5ba
zishanx <zishanalikhan53@gmail.com>
2026-06-25 21:29:31 +0530

    Done

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -8,7 +8,6 @@ export default async function PostPage({ params }: PageProps) {
     return (
         <>
             <h1>Post ID: {id}</h1>
-            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, consequatur aliquam repellendus nihil aut sint odio officia odit cupiditate, consequuntur veritatis amet reiciendis.</h2>
         </>
 
     )
